d82a2f48a5705a17cd27d8e9ea3b064d07d9af8e
Guilherme Albino <guilhermealbino33@gmail.com>
2022-01-27 14:53:01 -0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+import Shopify, { ApiVersion, AuthQuery } from '@shopify/shopify-api';
+require('dotenv').config();
+
+const app = express();
+
+const { API_KEY, API_SECRET_KEY, SCOPES, SHOP, HOST } = process.env;
+
+Shopify.Context.initialize({
+  API_KEY,
+  API_SECRET_KEY,
+  SCOPES: [SCOPES],
+  HOST_NAME: process.env.HOST.replace(/https:\/\//, ""),
+  IS_EMBEDDED_APP: {boolean},
+  API_VERSION: ApiVersion.{version} // all supported versions are available, as well as "unstable" and "unversioned"
+});
+// Storing the currently active shops in memory will force them to re-login when your server restarts. You should
+// persist this object in your app.
+const ACTIVE_SHOPIFY_SHOPS: { [key: string]: string | undefined } = {};
+
+// the rest of the example code goes here
+
+app.get("/", async (req, res) => {
+   // This shop hasn't been seen yet, go through OAuth to create a session
+  if (ACTIVE_SHOPIFY_SHOPS[SHOP] === undefined) {
+     // not logged in, redirect to login
+    res.redirect(`/login`);
+  } else {
+    res.send("Hello world!");
+    // Load your app skeleton page with App Bridge, and do something amazing!
+    res.end();
+  }
+});
+
+app.listen(3000, () => {
+  console.log('your app is now listening on port 3000');
+});
