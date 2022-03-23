ed8a677577b7addc6bdaa7e33c9386ef85d229f8
Guilherme Albino <guilhermealbino33@gmail.com>
2022-03-23 15:31:09 -0300

    login and auth route

--- src/index.ts
+++ src/index.ts
@@ -25,12 +25,37 @@ app.get("/", async (req, res) => {
    // This shop hasn't been seen yet, go through OAuth to create a session
   if (ACTIVE_SHOPIFY_SHOPS[SHOP] === undefined) {
      // not logged in, redirect to login
-    res.redirect(`/login`);
+
+    
   } else {
     res.send("Hello world!");
     // Load your app skeleton page with App Bridge, and do something amazing!
     res.end();
   }
+});
+app.get('/login', async (req, res) => {
+  let authRoute = await Shopify.Auth.beginAuth(
+    req,
+    res,
+    SHOP,
+    '/auth/callback',
+    false,
+  );
+  return res.redirect(authRoute);
+});
+app.get('/auth/callback', async (req, res) => {
+  try {
+    const session = await Shopify.Auth.validateAuthCallback(
+      req,
+      res,
+      req.query as unknown as AuthQuery,
+    ); // req.query must be cast to unkown and then AuthQuery in order to be accepted
+    ACTIVE_SHOPIFY_SHOPS[SHOP] = session.scope;
+    console.log(session.accessToken);
+  } catch (error) {
+    console.error(error); // in practice these should be handled more gracefully
+  }
+  return res.redirect(`/?host=${req.query.host}&shop=${req.query.shop}`); // wherever you want your user to end up after OAuth completes
 });
 
 app.listen(3000, () => {
