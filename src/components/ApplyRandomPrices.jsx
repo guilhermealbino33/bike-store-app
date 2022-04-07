fa99af50cab4d99684fa79750aaa6c2b85a37400
Guilherme Albino <guilhermealbino33@gmail.com>
2022-04-07 10:25:05 -0300

    Update ApplyRandomPrices.jsx

--- src/components/ApplyRandomPrices.jsx
+++ src/components/ApplyRandomPrices.jsx
@@ -17,3 +17,58 @@ const UPDATE_PRICE = gql`
     }
   }
 `;
+
+export function ApplyRandomPrices({ selectedItems, onUpdate }) {
+    const [hasResults, setHasResults] = useState(false);
+
+    const toast = hasResults && (
+        <Toast
+            content="Successfully updated"
+            onDismiss={() => setHasResults(false)}
+        />
+    );
+    const [mutateFunction, { data, loading, error }] = useMutation(UPDATE_PRICE);
+    if (loading) return <Loading />;
+
+    if (error) {
+        console.warn(error);
+        return <Banner status="critical">{error.message}</Banner>;
+    }
+
+    return (
+        <Frame>
+            {toast}
+
+            <Layout.Section>
+                <Stack distribution={"center"}>
+                    <Button
+                        primary
+                        textAlign={"center"}
+                        onClick={() => {
+                            let promise = new Promise((resolve) => resolve());
+                            for (const variantId in selectedItems) {
+                                const price = Math.random().toPrecision(3) * 10;
+                                const productVariableInput = {
+                                    id: selectedItems[variantId].variants.edges[0].node.id,
+                                    price: price,
+                                };
+
+                                promise = promise.then(() =>
+                                    mutateFunction({
+                                        variables: { input: productVariableInput },
+                                    })
+                                );
+                            }
+
+                            if (promise) {
+                                promise.then(() => onUpdate().then(setHasResults(true)));
+                            }
+                        }}
+                    >
+                        Randomize prices
+                    </Button>
+                </Stack>
+            </Layout.Section>
+        </Frame>
+    );
+}
