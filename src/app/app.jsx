6bcd249ee2f6c9e0957a5bdd53bfdd70b3e2ea7c
Guilherme Albino <guilhermealbino33@gmail.com>
2022-05-19 17:16:46 -0300

    Create app.jsx

---
+++ src/app/app.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { NavBar } from "../components";
+import { HomePage, EmptyStatePage } from "../pages";
+
+function App() {
+  return (
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/" exact element={<HomePage />} />
+        <Route path="/empty-state" exact element={<EmptyStatePage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
